fed2b3b80bd02f0a7b7902c3c7e308b95552867e
Ludvig Angenfelt <ludvig.angenfelt@telia.com>
2019-09-15 18:25:37 +0200

    Added default layout and alternate pages. Set basic CSS design.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const expressHandlebars = require('express-handlebars')
+const bodyParser = require('body-parser')
 
 const app = express()
 
@@ -9,22 +10,29 @@ app.engine("hbs", expressHandlebars ({
     defaultLayout: "main.hbs"
 }))
 
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
+
+app.get("/", function(request, response){
+    response.render("homepage.hbs")
+})
+
+app.get("/about", function(request, response){
+    response.render("about.hbs")
+})
+
+app.get("/contact", function(request, response){
+    response.render("contact.hbs")
+})
+
+app.get("/write-review", function(request, response){
+    const model = {
+        validationErrors: []
+    }
+
+    response.render("write-review.hbs", model)
+})
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-app.listen(8000)
+app.listen(8080)
